3300b992f240ab853bb07e27809e4b5f6bc1df5f
tennyccc <89793227+tennyccc@users.noreply.github.com>
2021-09-02 10:24:05 -0400

    Add files via upload

--- Draw Stars.cpp
+++ Draw Stars.cpp
@@ -1,32 +1,34 @@
-// Draw Stars.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
     int Number = 0, row = 0, star = 0;
- 
-    cout << "How many rows do you want to draw?" << endl;
-    cin >> Number;
-    while (!cin) {
+    string Userchoice;
+
+    do {
+
+        cout << "How many rows do you want to draw?" << endl;
+        cin >> Number;
+        while (!cin) {
             cout << "Invalid Input. Please enter a valid integer to draw stars: ";
             cin.clear();
             cin.ignore();
             cin >> Number;
-    }
-
-    for (row = 0; row <= Number; row++) {
-        for (star = 0; star < row; star++) {
-
-                cout << "*";
         }
 
+        for (row = 0; row <= Number; row++) {
+            for (star = 0; star < row; star++) {
+                cout << "*";
+            }
+
             cout << "\n";
+        }
+        cout << "Do you want to continue? \n";
+        cout << "Enter 'Yes' to contine, any other keys to quit.\n";
+        cin >> Userchoice;
+    }  while (Userchoice == "Yes");
 
-    }
-
-              
- return 0;
+    return 0;
 }
